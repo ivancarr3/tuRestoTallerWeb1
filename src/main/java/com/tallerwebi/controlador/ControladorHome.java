5bd13efd648bde73f799cf0d27e1471b997b446f
Genesis Alexa López <glopez@topgroup.com>
2024-05-07 00:18:41 -0300

    funcion de busqueda

--- src/main/java/com/tallerwebi/controlador/ControladorHome.java
+++ src/main/java/com/tallerwebi/controlador/ControladorHome.java
@@ -1,35 +1,52 @@
 package com.tallerwebi.controlador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tallerwebi.dominio.Restaurante;
+import com.tallerwebi.dominio.excepcion.RestauranteNoEncontrado;
 import com.tallerwebi.servicio.ServicioHome;
-import com.tallerwebi.servicio.ServicioHome;
+import com.tallerwebi.servicio.ServicioRestaurante;
 
 @Controller
 public class ControladorHome {
 	
 	
 	private ServicioHome servicioHome;
+	private ServicioRestaurante servicioRestaurante;
 	
 	@Autowired
-    public ControladorHome(ServicioHome servicioHome){
+    public ControladorHome(ServicioHome servicioHome, ServicioRestaurante servicioRestaurante){
         this.servicioHome = servicioHome;
+        this.servicioRestaurante = servicioRestaurante;
     }
+	
 	
 
      
 	 @RequestMapping(path = "/home", method = RequestMethod.GET)
-	    public ModelAndView irAHome() {
+	    public ModelAndView irAHome(@RequestParam(required = false) String busqueda) throws RestauranteNoEncontrado {
+		 if (busqueda != null) {
+			 ModelMap model = new ModelMap();
+			 List<Restaurante> restaurantes = servicioRestaurante.consultarRestaurantePorNombre(busqueda);
+			 System.out.println("Total de restaurantes que encontre: " + restaurantes.size());
+			 model.put("restaurantes",restaurantes);
+			 return new ModelAndView("home",model);
+		 } else {
 	        return new ModelAndView("home");
+		 }
 	    }
 
 	    @RequestMapping(path = "/", method = RequestMethod.GET)
 	    public ModelAndView inicio() {
 	        return new ModelAndView("redirect:/home");
 	    }
+	    
 }
