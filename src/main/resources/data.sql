cb5dff787c45bce0b9918289717bf9bf224bc27a
mateQF <mateo.fortu@gmail.com>
2024-05-07 12:13:03 -0300

    sugerencia de vista para home en home_test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO usuario (id, email, password, rol) VALUES (null, 'test@unlam.edu.ar', 'test', 'ADMIN');
-INSERT INTO restaurante (id, nombre, estrellas, direccion, id_plato) VALUES (null, 'El club de la Milanesa', 5, 'Arieta 5000', 1);
+INSERT INTO restaurante (id, nombre, estrellas, direccion) VALUES (null, 'El club de la Milanesa', 5, 'Arieta 5000');
 INSERT INTO plato (id, nombre, precio, descripcion) VALUES (null, 'milanesa', 15000, 'milanesa rellena de amor');
